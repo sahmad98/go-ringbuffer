240072052c14c7364dad63a97a996094283f6f43
Saleem Ahmad <saleem.iitg@gmail.com>
2018-06-27 21:25:39 +0530

    Added Documentation

---
+++ README.md
@@ -0,0 +1,44 @@
+# ringbuffer
+--
+    import "salgo/ringbuffer"
+
+Simple RingBuffer pacakge Ringbuffer is non blocking for readers and writers,
+writers will overwrite older data in a circular fashion. Readers will read from
+the current position and update it.
+
+## Usage
+
+#### type RingBuffer
+
+```go
+type RingBuffer struct {
+	Size      int           // Size of the Ringbuffer
+	Container []interface{} // Array container of objects
+	Reader    int           // Reader position
+	Writer    int           // Writer Position
+}
+```
+
+RingBuffer Structure
+
+#### func  NewRingBuffer
+
+```go
+func NewRingBuffer(size int) *RingBuffer
+```
+Create a new RingBuffer of initial size "size" Returns a pointer to the new
+RingBuffer
+
+#### func (*RingBuffer) Read
+
+```go
+func (r *RingBuffer) Read() interface{}
+```
+Read single object from the RingBuffer
+
+#### func (*RingBuffer) Write
+
+```go
+func (r *RingBuffer) Write(v interface{})
+```
+Write object into the RingBuffer
--- ring_buffer.go
+++ ring_buffer.go
@@ -1,12 +1,20 @@
+// Simple RingBuffer pacakge
+// Ringbuffer is non blocking for readers and writers, writers will
+// overwrite older data in a circular fashion. Readers will read
+// from the current position and update it.
 package ringbuffer
 
+
+// RingBuffer Structure 
 type RingBuffer struct {
-	Size      int
-	Container []interface{}
-	Reader    int
-	Writer    int
+	Size      int            // Size of the Ringbuffer 
+	Container []interface{}  // Array container of objects 
+	Reader    int            // Reader position
+	Writer    int            // Writer Position
 }
 
+// Create a new RingBuffer of initial size "size"
+// Returns a pointer to the new RingBuffer
 func NewRingBuffer(size int) *RingBuffer {
 	rb := new(RingBuffer)
 	rb.Size = size
@@ -16,15 +24,21 @@ func NewRingBuffer(size int) *RingBuffer {
 	return rb
 }
 
+
+// Write object into the RingBuffer
 func (r *RingBuffer) Write(v interface{}) {
 	r.Container[r.Writer] = v
 	r.Writer = (r.Writer + 1) % r.Size
 }
 
+// Seek position of the reader by delta, delta can be positive as
+// well as negative
 func (r *RingBuffer) seekReader(delta int) {
 	r.Reader = (r.Reader + delta) % r.Size
 }
 
+
+// Read single object from the RingBuffer
 func (r *RingBuffer) Read() interface{} {
 	defer r.seekReader(1)
 	return r.Container[r.Reader]
